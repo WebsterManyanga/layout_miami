a91ec12b5a8ad8559adb023720e272791892d5b6
Webster <strategywebstermanyanga@gmail.com>
2022-09-21 02:21:15 +0200

    make build

--- README.md
+++
@@ -1,4 +0,0 @@
-# Bose Landing Page
-## Description: Landing page for Bose created using HTML, CSS and SASS.
-
-- [DEMO LINK](https://WebsterManyanga.github.io/layout_miami/)
--- src/scripts/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const form = document.getElementById('form');
-
-form.addEventListener('submit', function handleSubmit(event) {
-  event.preventDefault();
-  form.reset();
-});
